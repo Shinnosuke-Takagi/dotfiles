84b537cebf5b840ad49ac6c9939355c55f429e4d
Shinnosuke Takagi <shinnosuketakagi@Shinnosukes-MacBook-Air.local>
2022-02-16 18:02:32 +0900

    Add fish_greeting

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,20 @@
-set -g theme_color_scheme nord
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user no
-set -g theme_hide_hostname no
-set -g theme_powerline_fonts no
+#view
+set -g theme_display_date yes
+set -g theme_date_format "+%F %H:%M"
+set -g theme_display_git_default_branch yes
+set -g theme_color_scheme dark
 set -g theme_hostname always
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+#path
 set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
 set -gx PATH "$ANDROID_HOME/emulator" $PATH
 set -gx PATH "$ANDROID_HOME/tools" $PATH
 set -gx PATH "$ANDROID_HOME/tools/bin" $PATH
 set -gx PATH "$ANDROID_HOME/platform-tools" $PATH
+
+#peco setting
+set fish_plugins theme peco
+
+function fish_user_key_bindings
+  bind \cw peco_select_history
+end
---
+++ config/fish/functions/fish_greeting.fish
@@ -0,0 +1,5 @@
+function fish_greeting
+  set_color $fish_color_autosuggestion
+  echo "Hello, your name is Shinnosuke Takagi. Now, Let's programming!!"
+  set_color normal
+end
